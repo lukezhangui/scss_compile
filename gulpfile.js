6c66d461b39db317464403060c122ed50a085cd3
lukezhangui <luke.zhang@workopolis.com>
2014-07-04 13:32:39 -0400

    Adding source maps

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,34 @@
 // Load plugins
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+sass = require('gulp-sass');
+
+// settings
+var target_scss = './framework/standard.scss'; //path to the scss file or files you want to compile
+var output_path = 'compiled/'; //path to the output folder
 
 // Styles
 gulp.task('styles', function() {
-  return gulp.src('./framework/standard.scss')
-    .pipe(sass({errLogToConsole: true}))
-    .pipe(gulp.dest('compiled/'));
+
+  return gulp.src(target_scss)
+  // this on function processes the file path to Unix style backslash in order for sourcemaps to work
+  .on('data', function(file) {
+    var path = require('path');
+    if (process.platform == "win32") {
+      file.path = path.relative(".", file.path);
+      file.path = file.path.replace(/\\/g, "/");
+    }
+  }).pipe(sass({ //compiles sass with source map option on
+    errLogToConsole: true,
+    sourceComments: 'map',
+    sourceMap: 'sass'
+  }))
+  .pipe(gulp.dest(output_path));
 });
 
 // Default task
 gulp.task('default', ['watch', 'styles']);
 
+// Watch and build task
 gulp.task('watch', function() {
-  // Watch .scss files
   gulp.watch('./framework/standard.scss', ['styles']);
 });
